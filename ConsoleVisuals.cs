5996c4e7c1442109faa7124a65bcae590d323e8e
Morgan <103436411+MorganKryze@users.noreply.github.com>
2023-04-24 12:58:52 +0200

    First commit

---
+++ ConsoleVisuals.cs
@@ -0,0 +1,423 @@
+﻿using static System.Console;
+using static System.Threading.Thread;
+using static System.IO.File;
+using static System.ConsoleColor;
+using static System.ConsoleKey;
+
+namespace MDD_FlowerStore;
+
+/// <summary> The <see cref="ConsoleVisuals"/> classe contains all the visual elements for a console app. </summary>
+public static class ConsoleVisuals
+{
+    #region Private attributes
+    private const string titlePath = "title.txt";
+    private static string[] titleContent = ReadAllLines(titlePath);
+    private static int initialWindowWidth = WindowWidth;
+    private static int intialWindowHeight = WindowHeight;
+    private static (ConsoleColor, ConsoleColor) colorPanel = (White, Black);
+    private static (ConsoleColor, ConsoleColor) initialColorPanel = (colorPanel.Item1, colorPanel.Item2);
+    private static (ConsoleColor, ConsoleColor) terminalColorpanel = (ForegroundColor, BackgroundColor);
+    #endregion
+
+    #region Private properties
+    private static (string, string, string) defaultHeader => ("gauche", "milieu", "droite");
+    private static (string, string, string) defaultFooter => ("gauche", "milieu", "droite");
+    private static int TitleHeight => titleContent.Length;
+    private static int HeaderHeigth => TitleHeight ;
+    private static int FooterHeigth => WindowHeight - 1;
+    private static int ContentHeigth => HeaderHeigth + 2;
+    private static bool WindowManipulated => WindowWidth != initialWindowWidth || WindowHeight != intialWindowHeight;
+    #endregion
+
+    #region Enums
+    /// <summary> The <see cref="Placement"/> enum defines the placement of a string in the console. </summary>
+    public enum Placement
+    {
+        /// <summary> The string is placed at the left of the console. </summary>
+        Left,
+        /// <summary> The string is placed at the center of the console. </summary>
+        Center,
+        /// <summary> The string is placed at the right of the console. </summary>
+        Right
+    }
+    #endregion
+
+    #region Low level methods
+    /// <summary> this method changes the font color of the console. </summary>
+    public static void ChangeFont(ConsoleColor newfont)
+    {
+        colorPanel.Item1 = newfont;
+    }
+    private static void TryNegative(bool negative = false)
+    {
+        ForegroundColor = negative ? colorPanel.Item2 : colorPanel.Item1;
+        BackgroundColor = negative ? colorPanel.Item1 : colorPanel.Item2;
+    }
+    private static void WritePositionnedString(string str, Placement position = Placement.Center, bool negative = false, int line = -1, bool chariot = false)
+	{
+        TryNegative(negative);
+		if (line < 0) 
+            line = Console.CursorTop;
+		if (str.Length < Console.WindowWidth) 
+            switch (position)
+		    {
+		    	case (Placement.Left): 
+                    SetCursorPosition(0, line); 
+                    break;
+		    	case (Placement.Center): 
+                    SetCursorPosition((WindowWidth - str.Length) / 2, line); 
+                    break;
+		    	case (Placement.Right): 
+                    SetCursorPosition(WindowWidth - str.Length, line); 
+                    break;
+		    }
+		else 
+            SetCursorPosition(0, line);
+		if (chariot) 
+            WriteLine(str);
+        else 
+            Write(str);
+        TryNegative(default);
+	}
+    private static void ClearLine(int line)
+	{
+		TryNegative(default);
+		WritePositionnedString("".PadRight(Console.WindowWidth), Placement.Left, default, line);
+	}
+    /// <summary> This method clears a specified part of the console. </summary>
+    /// <param name="startIndex"> The index of the first line to clear. </param>
+    /// <param name="length"> The number of lines to clear. </param>
+    public static void ClearPanel(int startIndex = -1, int length = 1)
+    {
+        if (startIndex == -1)
+            startIndex = ContentHeigth;
+        for (int i = startIndex; i < startIndex + length; i++)
+            ClearLine(i);
+    }
+    /// <summary> This method clears the content of the console. </summary>
+    public static void ClearContent()
+    {
+        for (int i = ContentHeigth - 1; i < FooterHeigth; i++)
+            ClearLine(i);
+    }
+    private static void ClearAll()
+    {
+        colorPanel = terminalColorpanel;
+        for (int i = 0; i < WindowHeight; i++)
+            ContinuousPrint("".PadRight(WindowWidth), i, default, 100, 10);
+        Clear();
+        colorPanel = (White, Black);
+    }
+    private static void ContinuousPrint(string text, int line, bool negative = false, int stringTime = 2000, int endStringTime = 1000)
+    {
+        int t_interval = (int)(stringTime / text.Length);
+        for (int i = 0; i <= text.Length; i++)
+        {
+            string continuous = "";
+            for(int j = 0; j < i; j++) 
+                continuous += text[j];
+            continuous = continuous.PadRight(text.Length);
+            WritePositionnedString(continuous.BuildString(WindowWidth, Placement.Center), default, negative, line);
+
+            if(i != text.Length)
+                Sleep(t_interval);
+
+            if(KeyAvailable)
+            {
+                ConsoleKeyInfo keyPressed = ReadKey(true);
+                if(keyPressed.Key == Enter || keyPressed.Key == Escape)
+                {
+                    i = text.Length;
+                    break;
+                }
+            }
+        }
+        WritePositionnedString(text.BuildString(WindowWidth, Placement.Center), default, negative, line);
+        Sleep(endStringTime);
+    }
+    private static bool IsScreenUpdated()
+    {
+        if (WindowManipulated || colorPanel != initialColorPanel)
+        {
+            WriteFullScreen(true);
+            initialWindowWidth = WindowWidth;
+            initialColorPanel = (colorPanel.Item1, colorPanel.Item2);
+            return true;
+        }
+        return false;
+    }
+    #endregion
+
+    #region Mid level methods
+    /// <summary> This method prints a float matrix in the console. </summary>
+    /// <param name="matrix"> The matrix to print. </param>
+    /// <param name="currentPosition"> The current position of the cursor. </param>
+    /// <param name="line"> The line where the matrix will be printed. </param>
+    public static void WriteMatrix(float[,] matrix, Position currentPosition, int line)
+    {
+        for(int i = line; i < matrix.GetLength(0); i++)
+            ClearLine(i);
+        SetCursorPosition(0, line);
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Write("{0,"+((WindowWidth / 2) - (matrix.GetLength(1))) + "}","");
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                TryNegative(currentPosition.Equals(new Position(i, j)));
+                Write(matrix[i, j]);
+                TryNegative(default);
+                Write("  ");
+            }
+            WriteLine("");
+        }
+    }
+    /// <summary> This method prints the title of the console app. </summary>
+    public static void WriteTitle()
+    {
+        Clear();
+        SetCursorPosition(0, 0);
+        foreach (string line in titleContent)
+        {
+            WritePositionnedString(line.BuildString(WindowWidth, Placement.Center));
+            WriteLine("");
+        } 
+    }
+    /// <summary> This method prints a banner in the console. </summary>
+    /// <param name="banner"> The banner to print. </param>
+    /// <param name="header"> If true, the banner is printed at the top of the console. If false, the banner is printed at the bottom of the console. </param>
+    /// <param name="straight"> If true, the title is not continuously printed. </param>
+    public static void WriteBanner((string, string, string)? banner = null, bool header = true, bool straight = false)
+	{
+        (string, string, string) newBanner = banner ??= header ? defaultHeader : defaultFooter;
+
+		TryNegative(true);
+		string strBanner = newBanner.Item2.BuildString(Console.WindowWidth, Placement.Center, true);
+		strBanner = strBanner.Substring(0, strBanner.Length - newBanner.Item3.Length) + newBanner.Item3;
+		strBanner = newBanner.Item1 + strBanner.Substring(newBanner.Item1.Length);
+        if (straight) 
+            WritePositionnedString(strBanner, default, true, header ? HeaderHeigth : FooterHeigth);
+        else
+		    ContinuousPrint(strBanner, header ? HeaderHeigth : FooterHeigth, true);
+		TryNegative();
+	}
+    /// <summary> This method prints a full screen in the console. </summary>
+    /// <param name="straight"> If true, the title is not continuously printed. </param>
+    /// <param name="header"> The header of the screen. </param>
+    /// <param name="footer"> The footer of the screen. </param>
+    public static void WriteFullScreen(bool straight = false, (string, string, string)? header = null, (string, string, string)? footer = null)
+    {
+        header ??= defaultHeader;
+        footer ??= defaultFooter;
+        CursorVisible = false;
+        WriteTitle();
+        WriteBanner(header, true, straight);
+        WriteBanner(footer, false, straight);
+        ClearContent();
+        if (!straight) 
+            LoadingScreen("[ Chargement ... ]");
+        ClearContent();
+    }
+    #endregion
+
+    #region High level methods
+    /// <summary> This method prints a message in the console and gets a string written by the user. </summary>
+    /// <param name="message"> The message to print. </param>
+    /// <param name="line"> The line where the message will be printed. </param>
+    /// <returns> The string written by the user. </returns>
+    public static string WritePrompt(string message, int line = -1)
+    {
+        if (line == -1) 
+            line = ContentHeigth;
+        if (!IsScreenUpdated())
+            ClearPanel(line, 3);
+
+        ContinuousPrint(message.BuildString(message.Length, Placement.Center), line, default, 1500, 50);
+        string prompt = "";
+        do
+        {
+            ClearLine(line + 1);
+            Write("{0," + ((WindowWidth / 2) - (message.Length / 2) + 2) + "}", "> ");
+            CursorVisible = true;
+            prompt = ReadLine() ?? "";
+            CursorVisible = false;
+        } while (prompt is "");
+        ClearPanel(line, 3);
+        return prompt;
+    }
+    /// <summary> This method prints a paragraph in the console. </summary>
+    /// <param name="text"> The lines of the paragraph. </param>
+    /// <param name="negative"> If true, the paragraph is printed in the negative colors. </param>
+    /// <param name="line"> The height of the paragraph. </param>
+    public static void WriteParagraph(IEnumerable<string> text, bool negative = false, int line = -1)
+	{
+        IsScreenUpdated();
+        if (line == -1)
+            line =  ContentHeigth;
+            ClearPanel(line, text.Count());
+
+        TryNegative(negative);
+		int maxLength = text.Count() > 0 ? text.Max(s => s.Length) : 0;
+		foreach (string str in text)
+		{
+			WritePositionnedString(str.BuildString(maxLength, Placement.Center), Placement.Center, negative, line++);
+			if (line >= WindowHeight - 1) 
+                break;
+		}
+        TryNegative(default);
+        //ClearPanel(line, text.Count());
+	}
+    /// <summary> This method prints a menu in the console and gets the choice of the user. </summary>
+    /// <param name="question"> The question to print. </param>
+    /// <param name="choices"> The choices of the menu. </param>
+    /// <param name="line"> The line where the menu is printed. </param>
+    /// <returns> The choice of the user. </returns>
+    public static int ScrollingMenu(string question, string[] choices, int line = -1)
+    {
+        IsScreenUpdated();
+        if (line == -1)
+            line = ContentHeigth;
+
+        int currentPosition = 0;
+        int maxLength = choices.Count() > 0 ? choices.Max(s => s.Length) : 0;
+        for (int i = 0; i < choices.Length; i++) 
+            choices[i] = choices[i].PadRight(maxLength);
+
+        ContinuousPrint(question, line, default, 1500, 50);
+        while (true)
+        {
+            string[] currentChoice = new string[choices.Length];
+            for (int i = 0; i < choices.Length; i++)
+            {
+                if (i == currentPosition)
+                {
+                    currentChoice[i] = $" ▶ {choices[i]}  ";
+                    WritePositionnedString(currentChoice[i], Placement.Center, true, line + 2 + i);
+                    continue;
+                }
+                currentChoice[i] = $"   {choices[i]}  ";
+                WritePositionnedString(currentChoice[i], Placement.Center, false, line + 2 + i);
+            }
+            switch (ReadKey(true).Key)
+            {
+                case UpArrow: case Z: 
+                    if (currentPosition == 0) 
+                        currentPosition = choices.Length - 1; 
+                    else if (currentPosition > 0)
+                        currentPosition--; 
+                        break;
+                case DownArrow: case S: 
+                    if (currentPosition == choices.Length - 1) 
+                        currentPosition = 0;  
+                    else if (currentPosition < choices.Length - 1) 
+                        currentPosition++; 
+                        break;
+                case Enter: 
+                    ClearPanel(line, choices.Length + 2);
+                    return currentPosition;
+                case Escape: 
+                    ClearPanel(line, choices.Length + 2);
+                    return -1;
+            }
+        }
+    }
+    /// <summary> This method prints a menu in the console and gets the choice of the user. </summary>
+    /// <param name="question"> The question to print. </param>
+    /// <param name="min"> The minimum value of the number. </param>
+    /// <param name="max"> The maximum value of the number. </param>
+    /// <param name="start"> The starting value of the number. </param>
+    /// <param name="step"> The step of the number. </param>
+    /// <param name="line"> The line where the menu is printed. </param>
+    /// <returns> The number chose of the user. </returns>
+    public static float NumberSelector(string question, float min, float max, float start = 0,float step = 100, int line = -1)
+    {
+        IsScreenUpdated();
+        if (line == -1)
+            line = ContentHeigth;
+        float currentNumber = start;
+        ContinuousPrint(question, line, default, 1500, 50);
+        while (true)
+        {
+            WritePositionnedString($" ▶ {(float)Math.Round(currentNumber, 1)} ◀ ", Placement.Center, true, line + 2);
+            
+            switch (ReadKey(true).Key)
+            {
+                case UpArrow: case Z: 
+                    if (currentNumber + step <= max)
+                        currentNumber += step;
+                    else if (currentNumber + step > max)
+                        currentNumber = min;
+                    break;
+                case DownArrow: case S: 
+                    if (currentNumber - step >= min)
+                        currentNumber -= step;
+                    else if (currentNumber - step < min)
+                        currentNumber = max;
+                        break;
+                case Enter: 
+                    ClearPanel(line, 4);
+                    return currentNumber;
+                case Escape: 
+                    ClearPanel(line, 4);
+                    return -1;
+            }
+            Sleep(1);
+            ClearLine(line +2);
+        }
+    }
+    
+    /// <summary> This method prints a loading screen in the console. </summary>
+    /// <param name="text"> The text to print. </param>
+    public static void LoadingScreen(string text)
+    {
+        if(!IsScreenUpdated())
+            ClearPanel(ContentHeigth, 3);
+        WritePositionnedString(text.BuildString(WindowWidth, Placement.Center), default, default, ContentHeigth, true);
+        string loadingBar = "";
+            for(int j = 0; j < text.Length; j++) 
+                loadingBar += '█';
+        ContinuousPrint(loadingBar, ContentHeigth + 2);
+    }
+    /// <summary> This method exits the program. </summary>
+    public static void ProgramExit()
+    {
+        LoadingScreen("[ Sortie du programme ... ]");
+        ClearAll();
+        CursorVisible = true;
+        Environment.Exit(0);
+    }
+    #endregion
+
+    #region Extensions  
+    /// <summary> This method builds a string with a specific size and a specific placement. </summary>
+    /// <param name="str"> The string to build. </param>
+    /// <param name="size"> The size of the string. </param>
+    /// <param name="position"> The placement of the string. </param>
+    /// <param name="truncate"> If true, the string is truncated if it is too long. </param>
+    /// <returns> The built string. </returns>
+    public static string BuildString(this string str, int size, Placement position = Placement.Center, bool truncate = true)
+	{
+		int padding = size - str.Length;
+        if (truncate && padding < 0) 
+            switch (position)
+		    {
+		    	case (Placement.Left): 
+                    return str.Substring(0, size);
+		    	case (Placement.Center): 
+                    return str.Substring((- padding) / 2, size);
+		    	case (Placement.Right): 
+                    return str.Substring(- padding, size);
+		    }
+        else 
+		    switch (position)
+		    {
+		    	case (Placement.Left):
+		    		return str.PadRight(size);
+		    	case (Placement.Center):
+		    		return str.PadLeft(padding / 2 + padding % 2 + str.Length).PadRight(padding + str.Length);
+		    	case (Placement.Right):
+		    		return str.PadLeft(size);
+		    }
+		return str;
+	}
+    #endregion
+}
---
+++ Program.cs
@@ -0,0 +1,147 @@
+﻿using MySql.Data.MySqlClient;
+using static System.Console;
+using static MDD_FlowerStore.ConsoleVisuals;
+
+namespace MDD_FlowerStore;
+
+public class Program
+{
+    public static MySqlConnection connection = new MySqlConnection("server=localhost;user id=mdd;password=mdd;database=PROJECT");
+    public static string mdpGerant = "securite";
+    public static Move execution = Move.MainMenu;
+    public static Profil user;
+    public static void Main(string[] args)
+    {
+        connection.Open();
+        WriteFullScreen(default);
+
+        MainMenu:
+
+        MainMenu();
+        if (execution == Move.Exit)
+            goto Exit;
+
+        user = Authentification(LogIn());
+        WriteParagraph(new string[]{ "Vous êtes connecté en tant que " + user});
+        ReadKey(true);
+
+
+        Options:
+
+        
+        Exit:
+
+        connection.Close();
+        ProgramExit();
+    }
+    public enum Move
+    {
+        MainMenu,
+        Options,
+        Back,
+        Exit
+    }
+    public static void MainMenu()
+    {
+        switch (ScrollingMenu("Bienvenue dans l'interface utilisateur de la boutique de M.BelleFleur, veuillez choisir votre prochaine action.", new string[]{"Authentification", "Options", "Quitter"}))
+        {
+            case 0:
+                user = Authentification(LogIn());
+                break;
+            case 1:
+                //Options();
+                break;
+            case 2:
+                execution = Move.Exit;
+                break;
+            default:
+                break;
+        }
+    }
+    public enum Profil
+    {
+        Gerant,
+        Client,
+        NonDefini
+    }
+    public static Profil LogIn()
+    {
+        switch(ScrollingMenu("Veuillez choisir votre statut :", new string[] { "Gerant", "Client" }))
+        {
+            case 0:
+                return Profil.Gerant;
+            case 1:
+                return Profil.Client;
+            default:
+                return Profil.NonDefini;
+        }
+    }
+    public static Profil Authentification(Profil utilisateur)
+    {
+        switch (utilisateur)
+        {
+            case Profil.Gerant:
+                MdpGerant:
+                string mdp1 = WritePrompt("Veuillez saisir le mot de passe pour s'authentifier : ");
+                if (mdp1 == mdpGerant)
+                    return Profil.Gerant;
+                else
+                    switch(ScrollingMenu("Mot de passe incorrect, réessayer ?", new string[] { "Oui", "Non" }))
+                    {
+                        case 0:
+                            goto MdpGerant;
+                        case 1:
+                            return Profil.NonDefini;
+                        default:
+                            return Profil.NonDefini;
+                    }
+            case Profil.Client:
+                Email:
+                string email = WritePrompt("Veuillez saisir votre email : ");
+                string query = $"SELECT email FROM Client WHERE email = '{email}'";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                if (command.ExecuteScalar() == null)
+                    switch (ScrollingMenu("Email incorrect, réessayer ?", new string[] { "Oui", "Non" }))
+                    {
+                        case 0:
+                            goto Email;
+                        case 1:
+                            return Profil.NonDefini;
+                        default:
+                            return Profil.NonDefini;
+                    }
+                else
+                {
+                    MdpClient:
+                    string mdp2 = WritePrompt("Veuillez saisir votre mot de passe : ");
+                    query = $"SELECT mdp FROM Client WHERE email = '{email}'";
+                    command = new MySqlCommand(query, connection);
+                    if (command.ExecuteScalar().ToString() == mdp2)
+                        return Profil.Client;
+                    else
+                        switch (ScrollingMenu("Mot de passe incorrect, réessayer ?", new string[] { "Oui", "Non" }))
+                        {
+                            case 0:
+                                goto MdpClient;
+                            case 1:
+                                return Profil.NonDefini;
+                            default:
+                                return Profil.NonDefini;
+                        }
+                }
+            default:
+                return Profil.NonDefini;
+        }
+    }
+    /*
+        string query = "SELECT * FROM Boutique";
+        MySqlCommand command = new MySqlCommand(query, connection);
+
+        MySqlDataReader reader = command.ExecuteReader();
+        WriteLine($"{reader.GetName(0),-4} {reader.GetName(1),-10} {reader.GetName(2),10}");
+        while (reader.Read())
+        {
+            WriteLine($"{reader.GetInt32(0),-4} {reader.GetString(1),-10} {reader.GetString(2),10}");
+        }
+    */
+}
